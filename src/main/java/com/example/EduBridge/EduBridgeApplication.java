588d6ac239a9d21b4246ccb071c3310a63d8c8f5
zinebMachrouh <zinebmac49@gmail.com>
2024-10-28 11:54:40 +0100

    initialize project

---
+++ src/main/java/com/example/EduBridge/EduBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.EduBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduBridgeApplication.class, args);
+	}
+
+}
